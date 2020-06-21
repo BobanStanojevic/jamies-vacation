ad0b010bb054a2a6fff6069611c27926c48efc72
Boban Stanojevic <boban.stanojevic@cg.nl>
2020-06-21 14:13:59 +0200

    Updated public path

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '/jamies-vacation/' : '/',
   pluginOptions: {
     'style-resources-loader': {
       preProcessor: 'scss',
